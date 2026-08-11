da12712d4b4b9b6193d1af4057ef5118afc07466
MkDriss <driss.meskini@outlook.fr>
2026-08-11 19:09:05 +0200

    feat: puzzle research added #8

--- services/files/front/pages/puzzles/puzzles.js
+++ services/files/front/pages/puzzles/puzzles.js
@@ -1,52 +1,13 @@
 import * as accountService from '../../services/account-service.js';
 import { GATEWAY_URL } from '../../env.js';
 
-// Mobile
-
-if (accountService.isLoggedIn()) {
-
-    const snell_coins = document.getElementById('snell-coins');
-    snell_coins.textContent = accountService.getSnellCoins();
-    snell_coins.addEventListener('click', async (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        e.stopImmediatePropagation();
-
-        console.log('[BUY] Initiating checkout...');
-
-        const userId = accountService.getUserId();
-        if (!userId) {
-            console.error("User ID non trouvé");
-            return;
-        }
-
-        try {
-            const res = await accountService.authFetch(`${GATEWAY_URL}/api/user/shop/create-checkout`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ userId })
-            });
-
-            const data = await res.json();
-            if (data.url) {
-                window.location.href = data.url;
-            } else {
-                console.error("Pas d'URL Stripe reçue", data);
-            }
-        } catch (err) {
-            console.error('[BUY] Fetch error:', err);
-        }
-    });
-} else {
-    document.querySelector('.snell-coins-container').style.display = "none";
-}
-
+let puzzles_ = [];
 
 // Puzzles
 
-async function loadPuzzles() {
+async function fetchPuzzles() {
     try {
-        const puzzle_list = document.querySelector('.puzzles')
+
         const res = await accountService.authFetch(`${GATEWAY_URL}/api/game/puzzles`)
 
         if (!res.ok) {
@@ -56,10 +17,55 @@ async function loadPuzzles() {
 
             const { puzzles } = await res.json();
 
-            puzzles.forEach(puzzle => {
-                const item = document.createElement('div');
-                item.classList.add('puzzle-item');
-                item.innerHTML += `
+            puzzles_ = puzzles;
+
+            loadPuzzles();
+
+            // puzzles.forEach(puzzle => {
+            //     const item = document.createElement('div');
+            //     item.classList.add('puzzle-item');
+            //     item.innerHTML += `
+            //         <div class="puzzle-item-left">
+            //             <div class="puzzle-ico-container">
+            //                 <img class="puzzle-ico" src="/assets/puzzle-piece-silver.svg">
+            //                 <span class="puzzle-number">${puzzle.id}</span>
+            //             </div>
+            //             <div class="puzzle-name">
+            //             ${puzzle.name}
+            //             </div>
+            //         </div>
+            //         <div class="puzzle-item-right">
+            //             <svg class="see-more-ico" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+            //                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            //                     <path d="m9 18 6-6-6-6"></path>
+            //             </svg>
+            //         </div>`
+
+            //     item.addEventListener("click", () => {
+            //         window.location.replace(`/pages/game/puzzle/index.html?id=${puzzle.id}`);
+            //     })
+
+            //     puzzle_list.appendChild(item);
+            // });
+
+        }
+
+    } catch (error) {
+        console.error('An Error Occured while fetching puzzles', error);
+    }
+}
+
+function loadPuzzles(query = "") {
+    try {
+        const puzzle_list = document.querySelector('.puzzles')
+        const displayed_puzzles = puzzles_.filter(p => p.name.toLowerCase().includes(query.toLocaleLowerCase()));
+
+        puzzle_list.replaceChildren();
+
+        displayed_puzzles.forEach(puzzle => {
+            const item = document.createElement('div');
+            item.classList.add('puzzle-item');
+            item.innerHTML += `
                     <div class="puzzle-item-left">
                         <div class="puzzle-ico-container">
                             <img class="puzzle-ico" src="/assets/puzzle-piece-silver.svg">
@@ -76,27 +82,35 @@ async function loadPuzzles() {
                         </svg>
                     </div>`
 
-                item.addEventListener("click", () => {
-                    window.location.replace(`/pages/game/puzzle/index.html?id=${puzzle.id}`);
-                })
+            item.addEventListener("click", () => {
+                window.location.replace(`/pages/game/puzzle/index.html?id=${puzzle.id}`);
+            })
 
-                puzzle_list.appendChild(item);
-            });
+            puzzle_list.appendChild(item);
 
-        }
-
+        });
     } catch (error) {
-        console.error('An Error Occured while fetching puzzles', error);
+        console.error("An Error occured while loading puzzles", error);
     }
+
 }
 
 function setupBtns() {
 
     try {
-        const mobile_logo = document.querySelector('.mobile-logo-ico');
-        mobile_logo.addEventListener('click', () => window.location.replace('/'))
+        const search_btn = document.querySelector('.search-puzzle-btn');
+        search_btn.addEventListener('click', () => search_puzzles())
+
+        const search_bar = document.querySelector('.search-puzzles');
+        search_bar.addEventListener('keydown', function (event) {
+            if (event.key === 'Enter') {
+                search_puzzles()
+            }
+        })
+
+
     } catch (error) {
-        console.error("Failed to setup leave btn", error);
+        console.error("Failed to setup search btn", error);
     }
 
     try {
@@ -105,7 +119,13 @@ function setupBtns() {
     } catch (error) {
         console.error("Failed to setup leave btn", error);
     }
+
+}
+
+function search_puzzles() {
+    const query = document.querySelector('.search-puzzles').value ?? ""
+    loadPuzzles(query);
 }
 
 setupBtns();
-await loadPuzzles();
+await fetchPuzzles();
