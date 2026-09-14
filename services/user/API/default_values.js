4d07ab3d526d08e56c17fe12b143f38c109c1cf0
mus <cyrilinveb@gmail.com>
2026-09-14 16:40:37 +0200

    hotfix: profile pictures

--- services/user/API/default_values.js
+++ services/user/API/default_values.js
@@ -36,6 +36,18 @@ export const ppList = [
         picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_lvjjRAVDQ-nBDq_4dy1xCyRjjDaHV-Tqcw&s",
         type: "profile-picture",
     },
+    {
+        id: 602,
+        name: "FISC",
+        picture: "https://play-lh.googleusercontent.com/USmEIorB00VniQ8OQ7QMw5Gp4TRXj_NsFrBE-xqIwvipF2AdwgFKBn63P_mpHwqD8do",
+        type: "profile-picture",
+    },
+    {
+        id: 603,
+        name: "Snell",
+        picture: "https://static.wikia.nocookie.net/logopedia/images/4/4a/Chess.com_2019_%28App_Icon%29.png/revision/latest/scale-to-width-down/250?cb=20221006103032",
+        type: "profile-picture",
+    }
 
 
 ]
